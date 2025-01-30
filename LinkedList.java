4632da68e99bbe4b699b3690e19e28176c4cf4ed
tkThek <gabriel.takeuchinav@post.runi.ac.il>
2025-01-30 20:11:22 +0200

    final attempt

--- LinkedList.java
+++ LinkedList.java
@@ -1,25 +1,24 @@
-/**
- * Represents a list of Nodes. 
- */
 public class LinkedList {
-	
+
 	private Node first; // pointer to the first element of this list
 	private Node last;  // pointer to the last element of this list
 	private int size;   // number of elements in this list
-	
+
 	/**
 	 * Constructs a new list.
-	 */ 
+	 */
+
 	public LinkedList () {
 		first = null;
-		last = first;
+		last = null;
 		size = 0;
 	}
-	
+
 	/**
 	 * Gets the first node of the list
 	 * @return The first node of the list.
-	 */		
+	 */
+
 	public Node getFirst() {
 		return this.first;
 	}
@@ -27,49 +26,54 @@ public class LinkedList {
 	/**
 	 * Gets the last node of the list
 	 * @return The last node of the list.
-	 */		
+	 */
+
 	public Node getLast() {
 		return this.last;
 	}
-	
+
 	/**
 	 * Gets the current size of the list
 	 * @return The size of the list.
-	 */		
+	 */
+
 	public int getSize() {
 		return this.size;
 	}
-	
+
 	/**
-	 * Gets the node located at the given index in this list. 
-	 * 
+	 * Gets the node located at the given index in this list.
+	 *
 	 * @param index
 	 *        the index of the node to retrieve, between 0 and size
 	 * @throws IllegalArgumentException
 	 *         if index is negative or greater than the list's size
 	 * @return the node at the given index
-	 */		
+	 */
+
 	public Node getNode(int index) {
-		if (index < 0 || index > size) {
-			throw new IllegalArgumentException(
-					"index must be between 0 and size");
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("index must be between 0 and size");
 		}
-		//// Replace the following statement with your code
-		return null;
+		Node current = first;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return current;
 	}
-	
+
 	/**
-	 * Creates a new Node object that points to the given memory block, 
+	 * Creates a new Node object that points to the given memory block,
 	 * and inserts the node at the given index in this list.
 	 * <p>
 	 * If the given index is 0, the new node becomes the first node in this list.
 	 * <p>
-	 * If the given index equals the list's size, the new node becomes the last 
+	 * If the given index equals the list's size, the new node becomes the last
 	 * node in this list.
-     * <p>
-	 * The method implementation is optimized, as follows: if the given 
-	 * index is either 0 or the list's size, the addition time is O(1). 
-	 * 
+	 * <p>
+	 * The method implementation is optimized, as follows: if the given
+	 * index is either 0 or the list's size, the addition time is O(1).
+	 *
 	 * @param block
 	 *        the memory block to be inserted into the list
 	 * @param index
@@ -77,102 +81,164 @@ public class LinkedList {
 	 * @throws IllegalArgumentException
 	 *         if index is negative or greater than the list's size
 	 */
+
 	public void add(int index, MemoryBlock block) {
-		//// Write your code here
+		if (index < 0 || index > size) {
+			throw new IllegalArgumentException("index must be between 0 and size");
+		}
+		if (index == 0) {
+			addFirst(block);
+		} else if (index == size) {
+			addLast(block);
+		} else {
+			Node current = getNode(index - 1);
+			Node newNode = new Node(block);
+			newNode.next = current.next;
+			current.next = newNode;
+			size++;
+		}
 	}
 
 	/**
 	 * Creates a new node that points to the given memory block, and adds it
 	 * to the end of this list (the node will become the list's last element).
-	 * 
+	 *
 	 * @param block
 	 *        the given memory block
 	 */
+
 	public void addLast(MemoryBlock block) {
-		//// Write your code here
+		Node newNode = new Node(block);
+		if (size == 0) {
+			first = newNode;
+			last = newNode;
+		} else {
+			last.next = newNode;
+			last = newNode;
+		}
+		size++;
 	}
-	
+
 	/**
-	 * Creates a new node that points to the given memory block, and adds it 
+	 * Creates a new node that points to the given memory block, and adds it
 	 * to the beginning of this list (the node will become the list's first element).
-	 * 
+	 *
 	 * @param block
 	 *        the given memory block
 	 */
+
 	public void addFirst(MemoryBlock block) {
-		//// Write your code here
+		Node newNode = new Node(block);
+		if (size == 0) {
+			first = newNode;
+			last = newNode;
+		} else {
+			newNode.next = first;
+			first = newNode;
+		}
+		size++;
 	}
 
 	/**
 	 * Gets the memory block located at the given index in this list.
-	 * 
+	 *
 	 * @param index
 	 *        the index of the retrieved memory block
 	 * @return the memory block at the given index
 	 * @throws IllegalArgumentException
 	 *         if index is negative or greater than or equal to size
 	 */
+
 	public MemoryBlock getBlock(int index) {
-		//// Replace the following statement with your code
-		return null;
-	}	
+		return getNode(index).block;
+	}
 
 	/**
 	 * Gets the index of the node pointing to the given memory block.
-	 * 
+	 *
 	 * @param block
 	 *        the given memory block
 	 * @return the index of the block, or -1 if the block is not in this list
 	 */
+
 	public int indexOf(MemoryBlock block) {
-		//// Replace the following statement with your code
+		Node current = first;
+		for (int i = 0; i < size; i++) {
+			if (current.block.equals(block)) {
+				return i;
+			}
+			current = current.next;
+		}
 		return -1;
 	}
 
 	/**
-	 * Removes the given node from this list.	
-	 * 
+	 * Removes the given node from this list.
+	 *
 	 * @param node
 	 *        the node that will be removed from this list
 	 */
+
 	public void remove(Node node) {
-		//// Write your code here
+		if (node == first) {
+			first = first.next;
+			if (first == null) {
+				last = null;
+			}
+		} else {
+			Node current = first;
+			while (current.next != node) {
+				current = current.next;
+			}
+			current.next = node.next;
+			if (node == last) {
+				last = current;
+			}
+		}
+		size--;
 	}
 
 	/**
 	 * Removes from this list the node which is located at the given index.
-	 * 
+	 *
 	 * @param index the location of the node that has to be removed.
 	 * @throws IllegalArgumentException
 	 *         if index is negative or greater than or equal to size
 	 */
+
 	public void remove(int index) {
-		//// Write your code here
+		remove(getNode(index));
 	}
 
 	/**
 	 * Removes from this list the node pointing to the given memory block.
-	 * 
+	 *
 	 * @param block the memory block that should be removed from the list
 	 * @throws IllegalArgumentException
 	 *         if the given memory block is not in this list
 	 */
 	public void remove(MemoryBlock block) {
-		//// Write your code here
-	}	
+		remove(getNode(indexOf(block)));
+	}
 
 	/**
 	 * Returns an iterator over this list, starting with the first element.
 	 */
-	public ListIterator iterator(){
+	public ListIterator iterator() {
 		return new ListIterator(first);
 	}
-	
+
 	/**
 	 * A textual representation of this list, for debugging.
 	 */
+
 	public String toString() {
-		//// Replace the following statement with your code
-		return "";
+		StringBuilder str = new StringBuilder();
+		ListIterator itr = iterator();
+		while (itr.hasNext()) {
+			str.append("(").append(itr.current.block.baseAddress).append(" , ").append(itr.current.block.length).append(") ");
+			itr.next();
+		}
+		return str.toString();
 	}
 }
--- MemorySpace.java
+++ MemorySpace.java
@@ -1,10 +1,5 @@
-/**
- * Represents a managed memory space. The memory space manages a list of allocated 
- * memory blocks, and a list free memory blocks. The methods "malloc" and "free" are 
- * used, respectively, for creating new blocks and recycling existing blocks.
- */
 public class MemorySpace {
-	
+
 	// A list of the memory blocks that are presently allocated
 	private LinkedList allocatedList;
 
@@ -13,16 +8,16 @@ public class MemorySpace {
 
 	/**
 	 * Constructs a new managed memory space of a given maximal size.
-	 * 
+	 *
 	 * @param maxSize
 	 *            the size of the memory space to be managed
 	 */
 	public MemorySpace(int maxSize) {
 		// initiallizes an empty list of allocated blocks.
 		allocatedList = new LinkedList();
-	    // Initializes a free list containing a single block which represents
-	    // the entire memory. The base address of this single initial block is
-	    // zero, and its length is the given memory size.
+		// Initializes a free list containing a single block which represents
+		// the entire memory. The base address of this single initial block is
+		// zero, and its length is the given memory size.
 		freeList = new LinkedList();
 		freeList.addLast(new MemoryBlock(0, maxSize));
 	}
@@ -30,64 +25,113 @@ public class MemorySpace {
 	/**
 	 * Allocates a memory block of a requested length (in words). Returns the
 	 * base address of the allocated block, or -1 if unable to allocate.
-	 * 
+	 *
 	 * This implementation scans the freeList, looking for the first free memory block 
 	 * whose length equals at least the given length. If such a block is found, the method 
 	 * performs the following operations:
-	 * 
+	 *
 	 * (1) A new memory block is constructed. The base address of the new block is set to
 	 * the base address of the found free block. The length of the new block is set to the value 
 	 * of the method's length parameter.
-	 * 
+	 *
 	 * (2) The new memory block is appended to the end of the allocatedList.
-	 * 
+	 *
 	 * (3) The base address and the length of the found free block are updated, to reflect the allocation.
 	 * For example, suppose that the requested block length is 17, and suppose that the base
 	 * address and length of the the found free block are 250 and 20, respectively.
 	 * In such a case, the base address and length of of the allocated block
 	 * are set to 250 and 17, respectively, and the base address and length
 	 * of the found free block are set to 267 and 3, respectively.
-	 * 
+	 *
 	 * (4) The new memory block is returned.
-	 * 
+	 *
 	 * If the length of the found block is exactly the same as the requested length, 
 	 * then the found block is removed from the freeList and appended to the allocatedList.
-	 * 
+	 *
 	 * @param length
 	 *        the length (in words) of the memory block that has to be allocated
 	 * @return the base address of the allocated block, or -1 if unable to allocate
 	 */
-	public int malloc(int length) {		
-		//// Replace the following statement with your code
-		return -1;
+	public int malloc(int length) {
+		Node current = freeList.getFirst();
+		if (current == null){
+			return -1;
+		}
+
+		while (current != null && current.block.length < length){
+			current = current.next;
+		}
+
+		if (current == null){
+			return -1;
+		}
+
+		MemoryBlock newBlock = new MemoryBlock(current.block.baseAddress, length);
+		if (current.block.length == length){
+			this.freeList.remove(current);
+		} else {
+			current.block.baseAddress += length;
+			current.block.length -= length;
+		}
+		this.allocatedList.addLast(newBlock);
+		return newBlock.baseAddress;
 	}
+
 
 	/**
 	 * Frees the memory block whose base address equals the given address.
 	 * This implementation deletes the block whose base address equals the given 
 	 * address from the allocatedList, and adds it at the end of the free list. 
-	 * 
+	 *
 	 * @param baseAddress
 	 *            the starting address of the block to freeList
 	 */
 	public void free(int address) {
-		//// Write your code here
+		Node current = allocatedList.getFirst();
+		if (allocatedList.getSize() == 0){
+			throw new IllegalArgumentException("index must be between 0 and size");
+		} else {
+			while (current.block.baseAddress != address && current != allocatedList.getLast()){
+				current = current.next;
+			}
+			if (current.block.baseAddress == address){
+				allocatedList.remove(current.block);
+				freeList.addLast(current.block);
+			}
+
+		}
 	}
-	
+
+
+
 	/**
 	 * A textual representation of the free list and the allocated list of this memory space, 
 	 * for debugging purposes.
 	 */
 	public String toString() {
-		return freeList.toString() + "\n" + allocatedList.toString();		
+		return freeList.toString() + "\n" + allocatedList.toString();
 	}
-	
+
 	/**
 	 * Performs defragmantation of this memory space.
 	 * Normally, called by malloc, when it fails to find a memory block of the requested size.
 	 * In this implementation Malloc does not call defrag.
 	 */
 	public void defrag() {
-		//// Write your code here
+		for (int i = 0; i < freeList.getSize(); i++){
+			MemoryBlock block = freeList.getBlock(i);
+			int result = block.baseAddress + block.length;
+			Node current = freeList.getFirst();
+			while (current != null){
+				MemoryBlock nextBlock = current.block;
+				if (nextBlock.baseAddress == result){
+					block.length += nextBlock.length;
+					freeList.remove(current);
+					defrag();
+					break;
+				}
+				current = current.next;
+			}
+		}
 	}
 }
